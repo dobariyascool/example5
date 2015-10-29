3569f09e0f4cb59bccbb458ad568a476ca1e3838
arraybit <info@arraybit.com>
2015-10-29 18:48:15 +0530

    POS

--- app/src/main/java/com/arraybit/parser/BusinessHoursJSONParser.java
+++ app/src/main/java/com/arraybit/parser/BusinessHoursJSONParser.java
@@ -17,12 +17,12 @@ import java.util.Locale;
 /// <summary>
 /// JSONParser for BusinessHoursTran
 /// </summary>
-public class BusinessHoursJSONParser
-{
+public class BusinessHoursJSONParser {
     public String SelectAllBusinessHoursTran = "SelectAllBusinessHoursTranPageWise";
+    public String SelectAllBusinessHoursTranById = "SelectAllBusinessHoursTranByBusinessMasterId";
 
-   // SimpleDateFormat sdfControlDateFormat = new SimpleDateFormat(Globals.DateFormat, Locale.US);
-    SimpleDateFormat sdfControlTimeFormat=new SimpleDateFormat(Globals.TimeFormat,Locale.US);
+    // SimpleDateFormat sdfControlDateFormat = new SimpleDateFormat(Globals.DateFormat, Locale.US);
+    SimpleDateFormat sdfControlTimeFormat = new SimpleDateFormat(Globals.TimeFormat, Locale.US);
     Date dt = null;
     SimpleDateFormat sdfTimeFormat = new SimpleDateFormat("HH:mm:ss", Locale.US);
     SimpleDateFormat sdfDateTimeFormat = new SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss", Locale.US);
@@ -32,13 +32,13 @@ public class BusinessHoursJSONParser
         try {
             if (jsonObject != null) {
                 objBusinessHoursTran = new BusinessHoursTran();
-                objBusinessHoursTran.setBusinessHoursTranId((short)jsonObject.getInt("BusinessHoursTranId"));
-                objBusinessHoursTran.setDayOfWeek((short)jsonObject.getInt("DayOfWeek"));
+                objBusinessHoursTran.setBusinessHoursTranId((short) jsonObject.getInt("BusinessHoursTranId"));
+                objBusinessHoursTran.setDayOfWeek((short) jsonObject.getInt("DayOfWeek"));
                 dt = sdfTimeFormat.parse(jsonObject.getString("OpeningTime"));
                 objBusinessHoursTran.setOpeningTime(sdfControlTimeFormat.format(dt));
                 dt = sdfTimeFormat.parse(jsonObject.getString("ClosingTime"));
                 objBusinessHoursTran.setClosingTime(sdfControlTimeFormat.format(dt));
-                objBusinessHoursTran.setlinktoBusinessMasterId((short)jsonObject.getInt("linktoBusinessMasterId"));
+                objBusinessHoursTran.setlinktoBusinessMasterId((short) jsonObject.getInt("linktoBusinessMasterId"));
             }
             return objBusinessHoursTran;
         } catch (JSONException e) {
@@ -54,13 +54,13 @@ public class BusinessHoursJSONParser
         try {
             for (int i = 0; i < jsonArray.length(); i++) {
                 objBusinessHoursTran = new BusinessHoursTran();
-                objBusinessHoursTran.setBusinessHoursTranId((short)jsonArray.getJSONObject(i).getInt("BusinessHoursTranId"));
-                objBusinessHoursTran.setDayOfWeek((short)jsonArray.getJSONObject(i).getInt("DayOfWeek"));
+                objBusinessHoursTran.setBusinessHoursTranId((short) jsonArray.getJSONObject(i).getInt("BusinessHoursTranId"));
+                objBusinessHoursTran.setDayOfWeek((short) jsonArray.getJSONObject(i).getInt("DayOfWeek"));
                 dt = sdfTimeFormat.parse(jsonArray.getJSONObject(i).getString("OpeningTime"));
                 objBusinessHoursTran.setOpeningTime(sdfControlTimeFormat.format(dt));
                 dt = sdfTimeFormat.parse(jsonArray.getJSONObject(i).getString("ClosingTime"));
                 objBusinessHoursTran.setClosingTime(sdfControlTimeFormat.format(dt));
-                objBusinessHoursTran.setlinktoBusinessMasterId((short)jsonArray.getJSONObject(i).getInt("linktoBusinessMasterId"));
+                objBusinessHoursTran.setlinktoBusinessMasterId((short) jsonArray.getJSONObject(i).getInt("linktoBusinessMasterId"));
                 lstBusinessHoursTran.add(objBusinessHoursTran);
             }
             return lstBusinessHoursTran;
@@ -82,8 +82,23 @@ public class BusinessHoursJSONParser
                 }
             }
             return lstBusinessHoursTran;
+        } catch (Exception ex) {
+            return null;
         }
-        catch (Exception ex) {
+    }
+
+    public ArrayList<BusinessHoursTran> SelectAllBusinessHoursTranById(int BusinessMasterId) {
+        ArrayList<BusinessHoursTran> lstBusinessHoursTran = null;
+        try {
+            JSONObject jsonResponse = Service.HttpGetService(Service.Url + this.SelectAllBusinessHoursTranById + "/" + BusinessMasterId);
+            if (jsonResponse != null) {
+                JSONArray jsonArray = jsonResponse.getJSONArray(this.SelectAllBusinessHoursTranById + "Result");
+                if (jsonArray != null) {
+                    lstBusinessHoursTran = SetListPropertiesFromJSONArray(jsonArray);
+                }
+            }
+            return lstBusinessHoursTran;
+        } catch (JSONException e) {
             return null;
         }
     }
