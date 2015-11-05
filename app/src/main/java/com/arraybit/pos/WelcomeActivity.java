9ca9e3e9800d80b068dbefc85330a3a62b4a13c8
arraybit <info@arraybit.com>
2015-11-05 11:10:27 +0530

    POS

--- app/src/main/java/com/arraybit/pos/WelcomeActivity.java
+++ app/src/main/java/com/arraybit/pos/WelcomeActivity.java
@@ -11,8 +11,6 @@ import android.view.MotionEvent;
 
 import com.arraybit.global.SharePreferenceManage;
 
-import java.util.Objects;
-
 
 public class WelcomeActivity extends Activity implements GestureDetector.OnGestureListener {
 
@@ -105,26 +103,23 @@ public class WelcomeActivity extends Activity implements GestureDetector.OnGestu
 
     }
 
+    @SuppressWarnings("StringEquality")
     @Override
     public boolean onFling(MotionEvent e1, MotionEvent e2, float velocityX, float velocityY) {
-
-        //if (e1.getX() > e2.getX() && velocityY < 1000) {
-
             objSharePreferenceManage = new SharePreferenceManage();
-            if (Objects.equals(objSharePreferenceManage.GetPreference("WaiterPreference", "UserTypeMasterId", WelcomeActivity.this),"1")) {
+            if(objSharePreferenceManage.GetPreference("WaiterPreference", "UserTypeMasterId", WelcomeActivity.this)=="1"){
                 Intent i = new Intent(WelcomeActivity.this, WaiterHomeActivity.class);
                 i.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
                 startActivity(i);
                 finish();
-
-            } else {
+            }
+            else
+            {
                 Intent i = new Intent(WelcomeActivity.this, WaitingActivity.class);
                 i.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
                 startActivity(i);
                 finish();
             }
-          //  return true;
-        //}
         return false;
     }
 }
