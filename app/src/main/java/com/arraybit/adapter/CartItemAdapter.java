8e61c11fc108ea1a07826992518ee614c200fbf2
arraybit <info@arraybit.com>
2016-01-04 09:59:34 +0530

    POS

---
+++ app/src/main/java/com/arraybit/adapter/CartItemAdapter.java
@@ -0,0 +1,178 @@
+package com.arraybit.adapter;
+
+import android.content.Context;
+import android.support.v4.content.ContextCompat;
+import android.support.v7.widget.RecyclerView;
+import android.view.Gravity;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+
+import com.arraybit.global.Globals;
+import com.arraybit.modal.ItemMaster;
+import com.arraybit.pos.R;
+import com.rey.material.widget.TextView;
+
+import java.util.ArrayList;
+
+public class CartItemAdapter extends RecyclerView.Adapter<CartItemAdapter.CartItemViewHolder>{
+
+    Context context;
+    ArrayList<ItemMaster> alItemMaster;
+    LayoutInflater layoutInflater;
+    View view;
+    CartItemOnClickListener objCartItemOnClickListener;
+    boolean isModifierChanged=true;
+
+    // Constructor
+    public CartItemAdapter(Context context, ArrayList<ItemMaster> result,CartItemOnClickListener objCartItemOnClickListener) {
+        this.context = context;
+        this.alItemMaster = result;
+        this.objCartItemOnClickListener = objCartItemOnClickListener;
+        this.layoutInflater = LayoutInflater.from(context);
+    }
+
+    @Override
+    public CartItemViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        view = layoutInflater.inflate(R.layout.row_cart_item, parent, false);
+        return new CartItemViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(CartItemViewHolder holder, int position) {
+        ItemMaster objItemMaster = alItemMaster.get(position);
+
+        holder.ivClose.setId(position);
+        holder.txtItem.setText(objItemMaster.getItemName());
+        holder.txtQty.setText(String.valueOf(objItemMaster.getQuantity()));
+        holder.txtAmount.setText(String.valueOf(Globals.dfWithPrecision.format(objItemMaster.getSellPrice())));
+        holder.txtRate.setText(String.valueOf(Globals.dfWithPrecision.format(objItemMaster.getSellPrice()/objItemMaster.getQuantity())));
+        if(isModifierChanged) {
+            if (objItemMaster.getAlOrderItemModifierTran().size() != 0) {
+                SetModifierLayout(objItemMaster.getAlOrderItemModifierTran(), holder);
+            }
+        }
+        if(objItemMaster.getRemark().equals("")){
+            holder.remarkLayout.setVisibility(View.GONE);
+        }
+        else{
+            holder.remarkLayout.setVisibility(View.VISIBLE);
+            if(objItemMaster.getRemark().substring(objItemMaster.getRemark().length()-1,objItemMaster.getRemark().length()).equals(","))
+            {
+                holder.txtRemark.setText(objItemMaster.getRemark().substring(0,objItemMaster.getRemark().length()-1));
+            }
+            else if(objItemMaster.getRemark().substring(objItemMaster.getRemark().length()-1,objItemMaster.getRemark().length()).equals(" ")){
+                holder.txtRemark.setText(objItemMaster.getRemark().substring(0,objItemMaster.getRemark().length()-2));
+            }
+            else{
+                holder.txtRemark.setText(objItemMaster.getRemark());
+            }
+        }
+    }
+
+    @Override
+    public int getItemCount() {
+        return alItemMaster.size();
+    }
+
+    public void RemoveData(int position){
+        if(alItemMaster.size()!=0 && position >= 0) {
+            isModifierChanged = false;
+            alItemMaster.remove(position);
+            notifyItemRemoved(position);
+            notifyDataSetChanged();
+            Globals.counter = Globals.counter - 1;
+        }
+    }
+
+    private void SetModifierLayout(ArrayList<ItemMaster> alOrderItemModifierTran,CartItemViewHolder holder){
+        TextView[] txtModifierName=new TextView[alOrderItemModifierTran.size()];
+        TextView[] txtModifierRate=new TextView[alOrderItemModifierTran.size()];
+        TextView[] txtModifierAmount=new TextView[alOrderItemModifierTran.size()];
+
+        for(int i=0;i<alOrderItemModifierTran.size();i++){
+
+            txtModifierName[i] = new TextView(context);
+            LinearLayout.LayoutParams layoutParams = new LinearLayout.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.WRAP_CONTENT);
+            txtModifierName[i].setLayoutParams(layoutParams);
+            txtModifierName[i].setTextSize(12f);
+            txtModifierName[i].setTextColor(ContextCompat.getColor(context, R.color.white_blur));
+            txtModifierName[i].setMaxLines(1);
+            txtModifierName[i].setText(alOrderItemModifierTran.get(i).getItemName());
+
+            txtModifierRate[i] = new TextView(context);
+            LinearLayout.LayoutParams txtModifierRateParams = new LinearLayout.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.WRAP_CONTENT);
+            txtModifierRate[i].setLayoutParams(txtModifierRateParams);
+            txtModifierRate[i].setTextSize(12f);
+            txtModifierRate[i].setGravity(Gravity.END);
+            txtModifierRate[i].setMaxLines(1);
+            txtModifierRate[i].setTextColor(ContextCompat.getColor(context, R.color.white_blur));
+            txtModifierRate[i].setText(String.valueOf(Globals.dfWithPrecision.format(alOrderItemModifierTran.get(i).getMRP())));
+
+            txtModifierAmount[i] = new TextView(context);
+            LinearLayout.LayoutParams txtModifierAmountParams = new LinearLayout.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.WRAP_CONTENT);
+            txtModifierAmount[i].setLayoutParams(txtModifierAmountParams);
+            txtModifierAmount[i].setTextSize(12f);
+            txtModifierAmount[i].setGravity(Gravity.END);
+            txtModifierAmount[i].setMaxLines(1);
+            txtModifierAmount[i].setTextColor(ContextCompat.getColor(context, R.color.white_blur));
+            txtModifierAmount[i].setText(String.valueOf(Globals.dfWithPrecision.format(alOrderItemModifierTran.get(i).getMRP())));
+
+            holder.modifierLayout.addView(txtModifierName[i]);
+            holder.modifierRateLayout.addView(txtModifierRate[i]);
+            holder.modifierAmountLayout.addView(txtModifierAmount[i]);
+
+        }
+
+    }
+
+    public interface CartItemOnClickListener{
+        void ImageViewOnClick(int position);
+    }
+    //endregion
+
+    //region ViewHolder
+    class CartItemViewHolder extends RecyclerView.ViewHolder {
+
+        TextView txtIndex, txtItem, txtQty, txtRate,txtAmount,txtRemark;
+        LinearLayout childLayout,modifierLayout,modifierRateLayout,modifierAmountLayout,remarkLayout;
+        ImageView ivClose;
+
+        public CartItemViewHolder(View itemView) {
+            super(itemView);
+
+            childLayout = (LinearLayout) itemView.findViewById(R.id.childLayout);
+            modifierLayout = (LinearLayout) itemView.findViewById(R.id.modifierLayout);
+            modifierRateLayout = (LinearLayout) itemView.findViewById(R.id.modifierRateLayout);
+            modifierAmountLayout = (LinearLayout) itemView.findViewById(R.id.modifierAmountLayout);
+            remarkLayout = (LinearLayout) itemView.findViewById(R.id.remarkLayout);
+
+            txtItem = (TextView) itemView.findViewById(R.id.txtItem);
+            txtAmount = (TextView) itemView.findViewById(R.id.txtAmount);
+            txtRate = (TextView) itemView.findViewById(R.id.txtRate);
+            txtQty = (TextView) itemView.findViewById(R.id.txtQty);
+            txtRemark = (TextView) itemView.findViewById(R.id.txtRemark);
+            ivClose = (ImageView) itemView.findViewById(R.id.ivClose);
+
+            childLayout.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+
+                }
+            });
+
+            ivClose.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    objCartItemOnClickListener.ImageViewOnClick(v.getId());
+                }
+            });
+        }
+    }
+
+}
+
+
+
---
+++ app/src/main/java/com/arraybit/adapter/OrderSummaryAdapter.java
@@ -0,0 +1,145 @@
+package com.arraybit.adapter;
+
+import android.content.Context;
+import android.support.v4.content.ContextCompat;
+import android.support.v7.widget.CardView;
+import android.support.v7.widget.RecyclerView;
+import android.view.Gravity;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.LinearLayout;
+
+import com.arraybit.modal.ItemMaster;
+import com.arraybit.modal.OrderMaster;
+import com.arraybit.pos.R;
+import com.rey.material.widget.TextView;
+
+import java.util.ArrayList;
+
+public class OrderSummaryAdapter extends RecyclerView.Adapter<OrderSummaryAdapter.OrderSummeryViewHolder> {
+
+    Context context;
+    ArrayList<ItemMaster> alOrderItemTran;
+    LayoutInflater layoutInflater;
+    View view;
+    ArrayList<OrderMaster> alOrderMaster;
+
+    public OrderSummaryAdapter(Context context, ArrayList<ItemMaster> result, ArrayList<OrderMaster> alOrderMasterId) {
+        this.context = context;
+        alOrderItemTran = result;
+        this.layoutInflater = LayoutInflater.from(context);
+        this.alOrderMaster = alOrderMasterId;
+    }
+
+    @Override
+    public OrderSummeryViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        view = layoutInflater.inflate(R.layout.row_order_summery, parent, false);
+        return new OrderSummeryViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(OrderSummeryViewHolder holder, int position) {
+        holder.txtOrderNumber.setText(String.valueOf(alOrderMaster.get(position).getOrderNumber()));
+        SetOrderItem(alOrderMaster.get(position).getOrderMasterId(), holder);
+    }
+
+    @Override
+    public int getItemCount() {
+        return alOrderMaster.size();
+    }
+
+    private void SetOrderItem(Long orderMasterId, OrderSummeryViewHolder holder) {
+
+        LinearLayout[] layout = new LinearLayout[alOrderItemTran.size()];
+        TextView[] txtName = new TextView[alOrderItemTran.size()];
+        TextView[] txtRate = new TextView[alOrderItemTran.size()];
+        TextView[] txtAmount = new TextView[alOrderItemTran.size()];
+        TextView[] txtQty = new TextView[alOrderItemTran.size()];
+
+        for (int i = 0; i < alOrderItemTran.size(); i++) {
+
+            if (orderMasterId == alOrderItemTran.get(i).getLinktoOrderMasterId()) {
+
+                layout[i] = new LinearLayout(context);
+                LinearLayout.LayoutParams layoutParams1 = new LinearLayout.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.WRAP_CONTENT);
+                layout[i].setLayoutParams(layoutParams1);
+                layout[i].setOrientation(LinearLayout.HORIZONTAL);
+
+                txtQty[i] = new TextView(context);
+                LinearLayout.LayoutParams txtQtyLayoutParams = new LinearLayout.LayoutParams(0, ViewGroup.LayoutParams.WRAP_CONTENT);
+                txtQtyLayoutParams.weight = 0.15f;
+                txtQty[i].setLayoutParams(txtQtyLayoutParams);
+                txtQty[i].setGravity(Gravity.START);
+                txtQty[i].setTextColor(ContextCompat.getColor(context, R.color.white_blur));
+
+                txtName[i] = new TextView(context);
+                LinearLayout.LayoutParams txtNameLayoutParams = new LinearLayout.LayoutParams(0, ViewGroup.LayoutParams.WRAP_CONTENT);
+                txtNameLayoutParams.weight = 0.5f;
+                txtName[i].setLayoutParams(txtNameLayoutParams);
+                txtName[i].setGravity(Gravity.START);
+                txtName[i].setTextColor(ContextCompat.getColor(context, R.color.white_blur));
+
+                txtRate[i] = new TextView(context);
+                LinearLayout.LayoutParams txtRateLayoutParams = new LinearLayout.LayoutParams(0, ViewGroup.LayoutParams.WRAP_CONTENT);
+                txtRateLayoutParams.weight = 0.22f;
+                txtRate[i].setLayoutParams(txtRateLayoutParams);
+                txtRate[i].setGravity(Gravity.END);
+                txtRate[i].setTextColor(ContextCompat.getColor(context, R.color.white_blur));
+
+                txtAmount[i] = new TextView(context);
+                LinearLayout.LayoutParams txtAmountLayoutParams = new LinearLayout.LayoutParams(0, ViewGroup.LayoutParams.WRAP_CONTENT);
+                txtAmountLayoutParams.weight = 0.3f;
+                txtAmount[i].setLayoutParams(txtAmountLayoutParams);
+                txtAmount[i].setGravity(Gravity.END);
+                txtAmount[i].setTextColor(ContextCompat.getColor(context, R.color.white_blur));
+
+                if (alOrderItemTran.get(i).getItemModifierIds().equals("0")) {
+                    txtQty[i].setTextSize(14f);
+                    txtName[i].setTextSize(14f);
+                    txtRate[i].setTextSize(14f);
+                    txtAmount[i].setTextSize(14f);
+
+                    txtQty[i].setVisibility(View.VISIBLE);
+                    txtQty[i].setText(String.valueOf(alOrderItemTran.get(i).getQuantity()));
+                    txtName[i].setText(alOrderItemTran.get(i).getItemName());
+                    txtRate[i].setText(String.valueOf(alOrderItemTran.get(i).getActualSellPrice()));
+                    txtAmount[i].setText(String.valueOf(alOrderItemTran.get(i).getSellPrice()));
+                } else {
+
+                    txtQty[i].setTextSize(12f);
+                    txtName[i].setTextSize(12f);
+                    txtRate[i].setTextSize(12f);
+                    txtAmount[i].setTextSize(12f);
+
+                    txtQty[i].setVisibility(View.INVISIBLE);
+                    txtName[i].setText(alOrderItemTran.get(i).getItemName());
+                    txtRate[i].setText(String.valueOf(alOrderItemTran.get(i).getActualSellPrice()));
+                    txtAmount[i].setText(String.valueOf(alOrderItemTran.get(i).getSellPrice()));
+                }
+
+                layout[i].addView(txtQty[i]);
+                layout[i].addView(txtName[i]);
+                layout[i].addView(txtRate[i]);
+                layout[i].addView(txtAmount[i]);
+
+                holder.orderItemLayout.addView(layout[i]);
+            }
+        }
+    }
+
+    class OrderSummeryViewHolder extends RecyclerView.ViewHolder {
+
+        CardView cvOrderItem;
+        TextView txtOrderNumber;
+        LinearLayout orderItemLayout;
+
+        public OrderSummeryViewHolder(View itemView) {
+            super(itemView);
+
+            cvOrderItem = (CardView) itemView.findViewById(R.id.cvOrderItem);
+            txtOrderNumber = (TextView) itemView.findViewById(R.id.txtOrderNumber);
+            orderItemLayout = (LinearLayout) itemView.findViewById(R.id.orderItemLayout);
+        }
+    }
+}
