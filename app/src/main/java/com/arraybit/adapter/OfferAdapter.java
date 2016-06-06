da49780a884239e7457e4a501ab4e665cfef2cae
arraybit <info@arraybit.com>
2016-06-06 19:01:31 +0530

    POS

--- app/src/main/java/com/arraybit/adapter/OfferAdapter.java
+++ app/src/main/java/com/arraybit/adapter/OfferAdapter.java
@@ -2,9 +2,13 @@ package com.arraybit.adapter;
 
 import android.annotation.SuppressLint;
 import android.content.Context;
+import android.os.Build;
 import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentTransaction;
 import android.support.v7.widget.CardView;
 import android.support.v7.widget.RecyclerView;
+import android.transition.Slide;
+import android.view.Gravity;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -12,6 +16,7 @@ import android.widget.ImageView;
 
 import com.arraybit.global.Globals;
 import com.arraybit.modal.OfferMaster;
+import com.arraybit.pos.OfferDetailFragment;
 import com.arraybit.pos.R;
 import com.rey.material.widget.TextView;
 import com.squareup.picasso.Picasso;
@@ -93,19 +98,19 @@ public class OfferAdapter extends RecyclerView.Adapter<OfferAdapter.OfferViewHol
                 @SuppressLint("RtlHardcoded")
                 @Override
                 public void onClick(View v) {
-//                    FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
-//                    OfferDetailFragment fragment = new OfferDetailFragment(alOfferMaster.get(getAdapterPosition()));
-//                    if (Build.VERSION.SDK_INT >= 21) {
-//                        Slide slideTransition = new Slide();
-//                        slideTransition.setSlideEdge(Gravity.RIGHT);
-//                        slideTransition.setDuration(500);
-//                        fragment.setEnterTransition(slideTransition);
-//                    } else {
-//                        fragmentTransaction.setCustomAnimations(R.anim.right_in, R.anim.left_out, 0, R.anim.right_exit);
-//                    }
-//                    fragmentTransaction.replace(R.id.offerFragment, fragment, context.getResources().getString(R.string.title_fragment_offer_detail));
-//                    fragmentTransaction.addToBackStack(context.getResources().getString(R.string.title_fragment_offer_detail));
-//                    fragmentTransaction.commit();
+                    FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+                    OfferDetailFragment fragment = new OfferDetailFragment(alOfferMaster.get(getAdapterPosition()));
+                    if (Build.VERSION.SDK_INT >= 21) {
+                        Slide slideTransition = new Slide();
+                        slideTransition.setSlideEdge(Gravity.RIGHT);
+                        slideTransition.setDuration(500);
+                        fragment.setEnterTransition(slideTransition);
+                    } else {
+                        fragmentTransaction.setCustomAnimations(R.anim.right_in, R.anim.left_out, 0, R.anim.right_exit);
+                    }
+                    fragmentTransaction.replace(R.id.offerFragment, fragment, context.getResources().getString(R.string.title_fragment_offer_detail));
+                    fragmentTransaction.addToBackStack(context.getResources().getString(R.string.title_fragment_offer_detail));
+                    fragmentTransaction.commit();
                 }
             });
 
